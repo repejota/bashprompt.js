e0d551673ed2f40bb5be6a9c76a040bd1cbc41a2
Raül Pérez <repejota@gmail.com>
2014-02-20 11:14:42 +0100

    First usable sample.

--- karma.conf.js
+++ karma.conf.js
@@ -1,45 +1,48 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
-  config.set({
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+module.exports = function (config) {
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+    'use strict';
 
-    // list of files / patterns to load in the browser
-    files: [
-      'lib/**/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
+    config.set({
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-    // list of files / patterns to exclude
-    exclude: [],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // list of files / patterns to load in the browser
+        files: [
+            'lib/**/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: [],
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: false,
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: [],
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
