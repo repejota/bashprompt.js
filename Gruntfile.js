384fc308b547504294e0c899e0146b9f51ba6971
Raül Pérez <repejota@gmail.com>
2014-02-20 11:33:35 +0100

    Renamed pybp.sh to bp.sh

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,51 @@
+//
+// BEGIN LICENSE BLOCK
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Raül Pérez
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+// END LICENSE BLOCK
+//
+
 module.exports = function (grunt) {
 
     'use strict';
 
     // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-
-    // Custom settings
-    var config = {
-    };
+    var matchdep = require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
     // Configuration tasks
     grunt.initConfig({
 
-        config: config,
+        bashlint: {
+            options: {
+                force: true
+            },
+            src: ['contrib/bp.sh']
+        },
 
         jshint: {
             build: {
                 files: {
-                    src: ['lib/**/*.js', 'Gruntfile.js']
+                    src: ['lib/**/*.js', 'Gruntfile.js', 'bin/bp']
                 }
             }
         },
@@ -26,7 +54,7 @@ module.exports = function (grunt) {
             prune: {
                 files: [{
                     dot: true,
-                    src: ['node_modules', 'bower_components']
+                    src: ['node_modules', 'doc/api']
                 }]
             }
         }
@@ -52,6 +80,7 @@ module.exports = function (grunt) {
 
     // Check task
     grunt.registerTask('check', [
+        'bashlint',
         'jshint'
     ]);
 
